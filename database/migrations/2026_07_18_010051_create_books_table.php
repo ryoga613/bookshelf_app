6512da1e8606202384831af62c753e63bba8f22d
Ryoga <ryogatoratora814@gmail.com>
2026-07-18 22:53:59 +0900

    マイグレーションファイル（create_bokks_table,create_genres_table,create_reviews_table,create_favorites-table,create_review_likes_table）の実装を行いました

---
+++ database/migrations/2026_07_18_010051_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->string('isbn');
+            $table->foreignId('genre_id')->constrained()->onDelete('cascade');
+            $table->date('published_at');
+            $table->string('image_url');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
