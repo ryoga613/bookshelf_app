5cc24b7533ba0a6fec7d7d52cf17744bc0d1059d
Ryoga <ryogatoratora814@gmail.com>
2026-08-11 15:20:32 +0900

    ApiBookControllerの実装を行いました

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/v1/books', [BookController::class, 'index']);
+Route::get('/v1/books/{book}', [BookController::class, 'show']);
+Route::post('/v1/books', [BookController::class, 'store']);
+Route::put('/v1/books/{book}', [BookController::class, 'update']);
+Route::delete('/v1/books/{book}', [BookController::class, 'delete']);
